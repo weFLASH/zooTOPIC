a984b6fb6dcdbd18877967459242965141c64e0d
seoyeoneee22 <nsy1382@naver.com>
2024-02-27 17:09:05 +0900

    developer라는 페이지 생성 + home 페이지 수정

--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return render (request,'home.html')
 # Create your views here.
+
+def introduce(request):
+    return render(request, 'developer.html')
